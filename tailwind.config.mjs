b62d1f415b6df50e77eeff0a39ee1de01ef468b5
Mike England <micheallengland@gmail.com>
2023-11-11 13:30:52 -0500

    Port tailwind config to project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,37 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			container: {
+				center: true,
+			},
+			fontFamily: {
+				sans: ['Mulish, sans-serif'],
+				mono: ['Fira Code, monospace']
+			},
+			colors: {
+				primary: {
+					purple: '#639',
+					purple1: '#020041',
+					purple2: '#170387'
+				},
+				secondary: {
+					pink: '#ff1ee5'
+				},
+				tertiary: {
+					teal: '#74edf1'
+				},
+				nuetral: {
+					white: '#fff',
+					dark: '#333'
+				}
+			},
+			zIndex: {
+				behind: '-1',
+				0: '0',
+				1: '1'
+			}
+		}
 	},
 	plugins: [],
 }
