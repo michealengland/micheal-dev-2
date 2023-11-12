29e2687c45ff6d77f8b50ddc8076af871b49aee0
Mike England <micheallengland@gmail.com>
2023-11-12 10:29:05 -0500

    Refactor PostHeader to use more meaningful dates

--- src/components/molecules/PostHeader/index.jsx
+++ src/components/molecules/PostHeader/index.jsx
@@ -2,12 +2,29 @@ import PropTypes from 'prop-types'
 import Heading from '../../atoms/Heading'
 import styles from './PostHeader.module.css'
 
-export default function PostHeader({content, title}) {
+export default function PostHeader({date, title, lastUpdated = null}) {
+  const formattedDate = date ? date.toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  }) : null
+
+  const formattedLastUpdated = lastUpdated ? lastUpdated.toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+  }) : null
+
   return (
     <header className={styles.postHeader}>
       <div className={styles.inner}>
         <Heading className={styles.title} tag="h1">{title}</Heading>
-        {content && <p className={styles.content}>{content}</p>}
+        { formattedDate &&
+          <p className={styles.date}>Published: <time datetime={date} itemprop="dateModified">{formattedDate}</time></p>
+        }
+        { formattedLastUpdated && (
+          <p className={styles.date}>Last updated: <time datetime={lastUpdated} itemprop="dateModified">{formattedLastUpdated}</time></p>
+        ) }
       </div>
     </header>
   )
@@ -15,7 +32,8 @@ export default function PostHeader({content, title}) {
 
 PostHeader.propTypes = {
   title: PropTypes.string,
-  content: PropTypes.string,
+  date: PropTypes.dateTime,
+  lastUpdated: PropTypes.dateTime,
 }
 
 
