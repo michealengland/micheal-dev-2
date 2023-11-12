0a79d0b68e742dcbe76edb8078546f59c9053288
Mike England <micheallengland@gmail.com>
2023-11-12 10:28:27 -0500

    Add null to className prop default

--- src/components/molecules/PostContent/index.jsx
+++ src/components/molecules/PostContent/index.jsx
@@ -3,7 +3,7 @@ import styles from './PostContent.module.css'
 import cn from 'classnames'
 import '@fontsource/fira-code/400.css'
 
-export default function PostContent({children, className}) {
+export default function PostContent({children, className=null}) {
   return (
     <section className={cn(styles.postContent, className)}>
       <div className={styles.inner}>{children}</div>
