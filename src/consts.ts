bf38168882ba728787a95236a8eb4fa00100d06f
Mike England <micheallengland@gmail.com>
2023-11-12 19:50:30 -0500

    Make updates to blog head and site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Micheal England Dev Blog';
+export const SITE_DESCRIPTION = 'Dive into my dev world: Coding insights, tech innovations, and workflow tips. Sharing my journey as a passionate developer.';
