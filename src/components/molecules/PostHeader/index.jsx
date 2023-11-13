d3af4a71368a4d0a1fbaafdfad5172e693b97d2a
Mike England <micheallengland@gmail.com>
2023-11-12 20:15:12 -0500

    Replace hardcoded functions with formatBlogDate

--- src/components/molecules/PostHeader/index.jsx
+++ src/components/molecules/PostHeader/index.jsx
@@ -1,19 +1,11 @@
 import PropTypes from 'prop-types'
 import Heading from '../../atoms/Heading'
 import styles from './PostHeader.module.css'
+import formatBlogDate from '../../../utilities/formatBlogDate'
 
 export default function PostHeader({date, title, lastUpdated, heroImage}) {
-  const formattedDate = date ? date.toLocaleDateString('en-us', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  }) : null
-
-  const formattedLastUpdated = lastUpdated ? lastUpdated.toLocaleDateString('en-us', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-  }) : null
+  const formattedDate = date ? formatBlogDate(date) : null
+  const formattedLastUpdated = lastUpdated ? formatBlogDate(lastUpdated) : null
 
   return (
     <header className={styles.postHeader}>
