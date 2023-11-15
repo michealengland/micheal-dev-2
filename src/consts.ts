8d0a45e1b1e5f33facc5f6b1e35b366087f7eb8d
Mike England <micheallengland@gmail.com>
2023-11-15 14:04:41 -0500

    Add twitter handle

--- src/consts.ts
+++ src/consts.ts
@@ -2,17 +2,18 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // SITE DATA
-export const SITE_TITLE = 'Micheal England Dev Blog';
-export const SITE_DESCRIPTION = 'Dive into my dev world: Coding insights, tech innovations, and workflow tips. Sharing my journey as a passionate developer.';
-export const SITE_BLOG_DESCRIPTION = "Micheal's personal blog on coding, tech trends, and productivity in software development.";
-export const SITE_URL = 'https://micheal.dev';
-export const SITE_LOGO = `${SITE_URL}/favicon.png`;
+export const SITE_TITLE = 'Micheal England Dev Blog'
+export const SITE_DESCRIPTION = 'Dive into my dev world: Coding insights, tech innovations, and workflow tips. Sharing my journey as a passionate developer.'
+export const SITE_BLOG_DESCRIPTION = "Micheal's personal blog on coding, tech trends, and productivity in software development."
+export const SITE_URL = 'https://micheal.dev'
+export const SITE_LOGO = `${SITE_URL}/favicon.png`
 // AUTHOR DATA
-export const AUTHOR_NAME = 'Micheal England';
-export const AUTHOR_ALT_NAME = 'Mike England';
-export const AUTHOR_POSITION = 'Software Developer';
-export const PUBLISHER_NAME = AUTHOR_NAME;
+export const AUTHOR_NAME = 'Micheal England'
+export const AUTHOR_ALT_NAME = 'Mike England'
+export const AUTHOR_POSITION = 'Software Developer'
+export const PUBLISHER_NAME = AUTHOR_NAME
 // SOCIAL LINKS
-export const SOCIAL_LINKEDIN = "https://www.linkedin.com/in/michealengland";
-export const SOCIAL_GITHUB = "https://www.github.com/michealengland";
-export const SOCIAL_TWITTER= "https://twitter.com/mikelikethebike"
+export const SOCIAL_LINKEDIN = "https://www.linkedin.com/in/michealengland"
+export const SOCIAL_GITHUB = "https://www.github.com/michealengland"
+export const SOCIAL_TWITTER = "https://twitter.com/mikelikethebike"
+export const SOCIAL_TWITTER_HANDLE = "@mikelikethebike"
