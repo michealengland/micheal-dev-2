91f7637df23d8215a5434fee7ae2ad073f46c17a
Mike England <micheallengland@gmail.com>
2023-11-17 11:00:19 -0500

    Add draft filtering to RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,8 +2,18 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
+const isProd = import.meta.env.PROD;
+
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	// Filter out posts that are marked as drafts from being published on production.
+	const filterDraftPosts = (posts=[{data: {isDraft: false}}], isProd) => (
+		! isProd ? posts :
+		posts.filter((post) => post.data.isDraft !== true)
+	)
+
+	// const posts = await getCollection('blog');
+	const posts = filterDraftPosts((await getCollection('blog')), isProd)
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
