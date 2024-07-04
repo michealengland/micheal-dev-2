f8e06367daac50ddb56b9d59c5ae2202ce507813
Mike England <micheallengland@gmail.com>
2024-07-04 10:48:54 -0400

    Update Hero to use typescript

--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -1,29 +1,31 @@
-import PropTypes from 'prop-types'
 import styles from './Hero.module.css'
 
-export default function Hero({title, content, img, imgAlt="", profileImg}) {
+interface HeroProps {
+  content?: string,
+  img?: string,
+  imgAlt?: string,
+  profileImg?: string,
+  title?: string,
+}
+
+export default function Hero({title, content, img, imgAlt="", profileImg}: HeroProps) {
   const backgroundImage = img && {backgroundImage: `url(${img})`}
 
   return (
     <section className={styles.hero} style={backgroundImage}>
       <div>
-        <img
-          alt={imgAlt}
-          className={styles.avatar}
-          height={512}
-          src={profileImg}
-          width={512}
-        />
+        {img && (
+          <img
+            alt={imgAlt}
+            className={styles.avatar}
+            height={512}
+            src={profileImg}
+            width={512}
+          />
+        )}
         {title && <h1 className={styles.title}>{ title }</h1>}
         {content && <p className={styles.content}>{ content }</p>}
       </div>
     </section>
   )
-}
-
-Hero.propTypes = {
-  content: PropTypes.string,
-  img: PropTypes.any,
-  imgAlt: PropTypes.string,
-  title: PropTypes.string,
 }
