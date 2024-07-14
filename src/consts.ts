e8ff96b7d82bbe005bc7d6eeddfdb2c8c6cddbc2
Mike England <micheallengland@gmail.com>
2024-07-14 10:58:20 -0400

    Update site SITE_TITLE const

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // SITE DATA
-export const SITE_TITLE = 'Micheal England Dev Blog'
+export const SITE_TITLE = 'Micheal England | JS, Node & React Insights'
 export const SITE_DESCRIPTION = 'Dive into my dev world: Coding insights, tech innovations, and workflow tips. Sharing my journey as a passionate developer.'
 export const SITE_BLOG_DESCRIPTION = "Micheal's personal blog on coding, tech trends, and productivity in software development."
 export const SITE_URL = 'https://micheal.dev'
