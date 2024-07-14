c6f10f9e17de6d03d31c3b1fc03726d38f5cfd16
Mike England <micheallengland@gmail.com>
2024-07-14 10:42:58 -0400

    Move ContentCTA to global footer

--- src/components/organisms/Layout/index.tsx
+++ src/components/organisms/Layout/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from './Layout.module.css'
 import Header from '../Header'
 import Footer from '../Footer'
+import ContentCTA from '../../molecules/ContentCTA'
+import contentCTAImage from '../../../content/assets/charlie-and-the-mechanical-keyboard-2000x1000.jpg'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -13,6 +15,13 @@ const Layout = ({children}: LayoutProps) => {
       <Header />
       <main id="layout" className={styles.layout}>
         {children}
+        <ContentCTA
+          content="Seeking a skilled JS, Node, or React developer? Connect with me on LinkedIn. Let's discuss how I can contribute to your team's success!"
+          img={contentCTAImage.src}
+          label="Connect"
+          title="Hire Me"
+          url="https://www.linkedin.com/in/michealengland/"
+        />
       </main>
       <Footer />
     </>
