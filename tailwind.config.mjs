39f593f30559c672757ef14ff9a3cd5a9fcbbf69
Mike England <micheallengland@gmail.com>
2025-03-16 10:43:39 -0400

    Incorporate rose-pin default colorscheme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -25,6 +25,19 @@ export default {
 				nuetral: {
 					white: '#fff',
 					dark: '#333'
+				},
+				rp: {
+					base: '#191724',
+					surface: '#2a273f',
+					overlay: '#393552',
+					subtle: '#908caa',
+					text: '#e0def4',
+					love: '#eb6f92',
+					gold: '#f6c177',
+					rose: '#ebbcba',
+					pine: '#31748f',
+					foam: '#9ccfd8',
+					iris: '#c4a7e7'
 				}
 			},
 			zIndex: {
